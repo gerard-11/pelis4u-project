3f24477dc15e8b14e2dd37287601a6b536b463e4
Gerardo J. Rito <grrdrito@gmail.com>
2026-04-01 18:39:37 -0600

    feat: connect all providers in main entry point

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,11 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import {QueryClientProvider} from "@tanstack/react-query";
+import {queryClient} from "@/lib/queryClient.ts";
+import {AuthProvider} from "@/components/AuthProvider.tsx";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
 
 createRoot(document.getElementById('root')!).render(
         <StrictMode>
-          
-            <App />
+            <QueryClientProvider client={queryClient}>
+                <AuthProvider>
+                    <App />
+                </AuthProvider>
+                <ReactQueryDevtools initialIsOpen={false} />
+            </QueryClientProvider>
         </StrictMode>
 
 )
